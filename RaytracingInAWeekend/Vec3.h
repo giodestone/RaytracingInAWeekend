aecdd22b1173eec898ad3eab7da4e318fab94c19
giodestone <37457035+giodestone@users.noreply.github.com>
2021-10-17 00:29:25 +0100

    Added Vec3 operators, utilities etc.

--- RaytracingInAWeekend/Vec3.h
+++ RaytracingInAWeekend/Vec3.h
@@ -3,30 +3,30 @@
 #include <cmath>
 #include <iostream>
 
+typedef double VectorRealDataType;
+
 /// <summary>
 /// Class for representing a point in 3D space, or a color (see aliases at bottom of this file).
 /// </summary>
 class Vec3
-{
-	typedef double RealDataType;
-	
-	RealDataType x;
-	RealDataType y;
-	RealDataType z;
+{	
+	VectorRealDataType x;
+	VectorRealDataType y;
+	VectorRealDataType z;
 
 public:
-	Vec3(const RealDataType x=0, const RealDataType y=0, const RealDataType z=0) : x(x), y(y), z(z) {}
+	Vec3(const VectorRealDataType x=0, const VectorRealDataType y=0, const VectorRealDataType z=0) : x(x), y(y), z(z) {}
 
-	RealDataType X() const { return x; }
-	RealDataType Y() const { return y; }
-	RealDataType Z() const { return z; }
+	VectorRealDataType X() const { return x; }
+	VectorRealDataType Y() const { return y; }
+	VectorRealDataType Z() const { return z; }
 
 	/// <summary>
 	/// For accessing the vector like an array.
 	/// </summary>
 	/// <param name="i"></param>
 	/// <returns></returns>
-	RealDataType operator[](size_t i) const
+	VectorRealDataType operator[](size_t i) const
 	{
 		switch (i)
 		{
@@ -50,7 +50,7 @@ public:
 	/// </summary>
 	/// <param name="i"></param>
 	/// <returns></returns>
-	RealDataType & operator[](size_t i)
+	VectorRealDataType & operator[](size_t i)
 	{
 		switch (i)
 		{
@@ -80,7 +80,7 @@ public:
 		return *this;
 	}
 	
-	Vec3& operator*= (const RealDataType scalar)
+	Vec3& operator*= (const VectorRealDataType scalar)
 	{
 		x *= scalar;
 		y *= scalar;
@@ -88,7 +88,7 @@ public:
 		return *this;
 	}
 
-	Vec3& operator/= (const RealDataType scalar)
+	Vec3& operator/= (const VectorRealDataType scalar)
 	{
 		x /= scalar;
 		y /= scalar;
@@ -96,12 +96,12 @@ public:
 		return *this;
 	}
 	
-	RealDataType GetLength() const
+	VectorRealDataType GetLength() const
 	{
 		return sqrt(GetLengthSquared());
 	}
 
-	RealDataType GetLengthSquared() const
+	VectorRealDataType GetLengthSquared() const
 	{
 		return x * x + y * y + z * z;
 	}
@@ -111,3 +111,61 @@ public:
 
 using Point3 = Vec3;
 using Color = Vec3;
+
+#pragma region VectorUtilityFunctions
+inline std::ostream& operator<<(std::ostream& out, const Vec3& v)
+{
+	return out << "(" << v.X() << ", " << v.Y() << ", " << v.Z() << ")";
+}
+
+inline Vec3 operator+ (const Vec3& lhs, const Vec3& rhs)
+{
+	return { lhs.X() + rhs.X(), lhs.Y() + rhs.Y(), lhs.Z() + rhs.Z() };
+}
+
+inline Vec3 operator- (const Vec3& lhs, const Vec3& rhs)
+{
+	return { lhs.X() - rhs.X(), lhs.Y() - rhs.Y(), lhs.Z() - rhs.Z() }; // does NOT fix a - b problems.
+}
+
+inline Vec3 operator* (const Vec3& lhs, const Vec3& rhs)
+{
+	return { lhs.X() * rhs.X(), lhs.Y() * rhs.Y(), lhs.Z() * rhs.Z() };
+}
+
+inline Vec3 operator* (const Vec3& lhs, const VectorRealDataType scalar)
+{
+	return { lhs.X() * scalar, lhs.Y() * scalar, lhs.Z() * scalar };
+}
+
+inline Vec3 operator* (const VectorRealDataType scalar, const Vec3& rhs)
+{
+	return { rhs.X() * scalar, rhs.Y() * scalar, rhs.Z() * scalar };
+}
+
+inline Vec3 operator/ (Vec3 v, VectorRealDataType scalar)
+{
+	return (1 / scalar) * v;
+}
+
+inline VectorRealDataType Dot(const Vec3& a, const Vec3& b)
+{
+	return a[0] * b[0] +
+		a[1] * b[1] +
+		a[2] * a[2];
+}
+
+inline Vec3 GetUnitVector(Vec3 v)
+{
+	return v / v.GetLength();
+}
+#pragma endregion VectorUtilityFunctions
+
+#pragma region ColorUtilityFunctions
+void WriteColor(std::ostream &out, Color color)
+{
+	out << static_cast<int> (255.999 * color[0]) << ' '
+		<< static_cast<int> (255.999 * color[1]) << ' '
+		<< static_cast<int> (255.999 * color[2]);
+}
+#pragma endregion ColorUtilityFunctions
